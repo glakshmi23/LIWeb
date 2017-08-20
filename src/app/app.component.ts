48463b1f0e75479aac708c9e694688c1a54e53cc
glakshmi23 <glakshmi23@gmail.com>
2017-08-21 01:43:42 +0530

    update of actual files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HomeComponent} from './home/home.component';
+import {LoginComponent} from './login/login.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { routes } from './app.routing';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    FooterComponent,
+    ServicesComponent,
+    ContactusComponent,
+    AboutusComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, routes
   ],
   providers: [],
   bootstrap: [AppComponent]
